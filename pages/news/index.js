23fee6e0dcd58eb5cbcde99eaedeb8fde8978d18
Alex Brilevski <brilevski.alex@gmail.com>
2026-01-25 12:04:32 +0300

    Links to hardcoded news details pages were added

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,6 +1,22 @@
+import Link from 'next/link';
+
 function NewsPage() {
   return (
-    <h1>News page</h1>
+    <>
+      <h1>News page</h1>
+      <ul>
+        <li>
+          <Link href={'/news/news-page-1'}>
+            News Page 1
+          </Link>
+        </li>
+        <li>
+          <Link href={'/news/news-page-1'}>
+            News Page 1
+          </Link>
+        </li>
+      </ul>
+    </>
   );
 }
 
