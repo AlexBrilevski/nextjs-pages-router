0e5d66732a50ab5e2cc5aefd4f5de37d939c7e2f
Alex Brilevski <brilevski.alex@gmail.com>
2026-01-26 21:32:13 +0300

    Outputting a list of meetups on the home page was implemented

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,26 @@
+import MeetupList from '../components/meetups/MeetupList';
 
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: 'The First meetup',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+    address: 'Some address 5, 12345 Some City',
+    description: 'This is the first meetup!'
+  },
+  {
+    id: 'm2',
+    title: 'The Second meetup',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+    address: 'Some address 5, 12345 Some City',
+    description: 'This is the second meetup!'
+  },
+];
+
+function HomePage() {
+  return (
+    <MeetupList meetups={DUMMY_MEETUPS} />
+  );
+}
+
+export default HomePage;
