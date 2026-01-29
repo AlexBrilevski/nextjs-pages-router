eb40fe69190ca0fb179bd3edd8db1bf9dfeac058
Alex Brilevski <brilevski.alex@gmail.com>
2026-01-29 21:36:03 +0300

    Basic components for displaying meetup details page were added

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,1 +1,14 @@
+import MeetupDetails from "../../components/meetups/MeetupDetail";
 
+function MeetupDetailsPage() {
+  return (
+    <MeetupDetails
+      image={'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg'}
+      title={'The First meetup'}
+      address={'Some address 5, 12345 Some City'}
+      description={'This is the first meetup!'}
+    />
+  );
+}
+
+export default MeetupDetailsPage;
