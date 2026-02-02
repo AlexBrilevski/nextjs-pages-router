f1be162ab8aeeaed8966565c8edd76fa76946f79
Alex Brilevski <brilevski.alex@gmail.com>
2026-02-02 21:08:58 +0300

    Fetching data on the meetup details page was implemented

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,37 +1,34 @@
+import sql from 'better-sqlite3';
+
 import MeetupDetails from "../../components/meetups/MeetupDetail";
 
 function MeetupDetailsPage(props) {
   return (
-    <MeetupDetails {...props.meetupData}/>
+    <MeetupDetails {...props.meetupData} />
   );
 }
 
 export async function getStaticPaths() {
+  const db = sql('meetups.db');
+  const meetups = db.prepare('SELECT * FROM meetups').all();
+
   return {
-    fallback: true,
-    paths: [
-      {
-        params: { meetupId: 'm1' },
-      },
-      {
-        params: { meetupId: 'm2' },
-      },
-    ]
+    fallback: false,
+    paths: meetups.map(meetup => ({ params: { meetupId: meetup.url } })),
   }
 }
 
 export async function getStaticProps(context) {
   const meetupId = context.params.meetupId;
 
+  const db = sql('meetups.db');
+  const meetupData = db.prepare('SELECT * FROM meetups WHERE url = ?').get(meetupId);
+
+  console.log(meetupData);
+
   return {
     props: {
-      meetupData: {
-        url: meetupId,
-        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
-        title: 'The First meetup',
-        address: 'Some address 5, 12345 Some City',
-        description: 'This is the first meetup!',
-      },
+      meetupData,
     },
   };
 }
